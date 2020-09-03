9e1a17f6d781d0d1bbf0b68d3fe38da5815ff9e9
James Truxon <truxonjm@cmog.org>
2020-09-03 10:52:15 -0400

    resolving /dev/nul issue -- was using windows standard not linux

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ echo "unlocking..."
 source ./git-unlock.sh
 
 echo "packaging:"
-cp $VALUES_FILE values.yaml 2>/dev/nul
+cp $VALUES_FILE values.yaml 2>/dev/null
 helm package .  --destination "/mnt/c/Dropbox/Root/Development/BitBucket/Helm/catalog/packaged"
 
 pushd "/mnt/c/Dropbox/Root/Development/BitBucket/Helm/catalog"
